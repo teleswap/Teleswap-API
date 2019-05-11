a78899523223fddcff52bb6187f250199bc47f37
Moin Uddin <U_Moin75@yahoo.com>
2019-05-10 21:59:09 -0400

    Create user model and implement authentication without JWT verification

---
+++ db/migrate/20190511004939_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.string :username
+      t.string :image_url, default: 'https://res.cloudinary.com/dg3xzcms5/image/upload/v1548039381/images/default-profile-icon-24.jpg'
+      t.string :password_digest
+
+      t.timestamps
+    end
+  end
+end

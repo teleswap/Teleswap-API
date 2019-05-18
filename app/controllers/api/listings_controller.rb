aff6661b2aa4cbbf0477e852a341877d6f176505
Moin Uddin <U_Moin75@yahoo.com>
2019-05-17 21:43:09 -0400

    Generate Offers and Listing Controllers

---
+++ app/controllers/api/listings_controller.rb
@@ -0,0 +1,19 @@
+class Api::ListingsController < ApplicationController
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+end
---
+++ app/controllers/api/offers_controller.rb
@@ -0,0 +1,18 @@
+class Api::OffersController < ApplicationController
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+  
+end
